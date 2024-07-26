f890775d369102ca2e822a31857e5ba7356de4c1
NdamukundaA <h.allan@alustudent.com>
2024-07-26 06:16:39 -0700

    C

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,15 +1,11 @@
-#!/usr/bin/python3
-"""
-Module 5-hbtn_header.py
-"""
-
-
-from sys import argv
 import requests
+import sys
 
+def main():
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
 
 if __name__ == "__main__":
-    url = argv[1]
-    le = requests.get(url)
-    la = le.headers.get('X-Request-Id')
-    print(la)
+    main()
+
